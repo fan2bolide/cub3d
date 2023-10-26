1f3b986b36f3151d7109f517a82bfff42547c05d
basil <basiljeannot@icloud.com>
2023-10-26 11:52:25 +0200

    Merge branch 'parsing' of /Users/basil/Desktop/42_projects/cub3d with conflicts.

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nfaust <nfaust@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: bajeanno <bajeanno@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 06:38:22 by nfaust            #+#    #+#             */
-/*   Updated: 2023/10/25 13:33:27 by bajeanno         ###   ########.fr       */
+/*   Updated: 2023/10/26 11:04:51 by bajeanno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,5 @@
 
 int	parsing(int argc, char **argv)
 {
-	t_data	*data;
-
-	if (argc != 2)
-		return (printf(ERR WRONG_ARG_N), 1);
-	data = get_data(argv);
-	if (!data)
-		return (1);
-	free_data(data);
 	return (0);
 }
