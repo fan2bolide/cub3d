7f29d4d18771ebe170b2c080ed12e7bf8f557ad1
Basile Jeannot <bajeanno@z4r6p3.42lyon.fr>
2023-11-26 10:17:55 +0100

    fixed segfault on school macs

--- srcs/rendering/render_view.c
+++ srcs/rendering/render_view.c
@@ -15,6 +15,8 @@
 int	cub_textures_render(t_cub *cub, int wall_height, int x,
 					t_position ray_collision)
 {
+	t_prtl_list	*curr;
+
 	if (cub->data->baj->is_activated \
 	&& get_time() - cub->data->baj->last_move > cub->data->baj->speed)
 	{
@@ -25,14 +27,12 @@ int	cub_textures_render(t_cub *cub, int wall_height, int x,
 	if (!cub->portals[x])
 		return (cub_texture_put(x, cub, wall_height, ray_collision), 1);
 	cub_texture_put(x, cub, wall_height, ray_collision);
-	cub->portals[x] = (t_prtl_list *)ft_dblstlast((t_dblist *)cub->portals[x]);
-	cub_portal_texture_put(x, cub, cub->portals[x]->portal->height, \
-								cub->portals[x]->portal->position);
-	while (cub->portals[x]->prev)
+	curr = (t_prtl_list *)ft_dblstlast((t_dblist *)cub->portals[x]);
+	while (curr)
 	{
-		cub->portals[x] = cub->portals[x]->prev;
-		cub_portal_texture_put(x, cub, cub->portals[x]->portal->height, \
-									cub->portals[x]->portal->position);
+		cub_portal_texture_put(x, cub, curr->portal->height, \
+								curr->portal->position);
+		curr = curr->prev;
 	}
 	return (1);
 }
