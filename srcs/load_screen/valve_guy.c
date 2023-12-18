f7d9b214541ff02c8d7f719369495f1bde598420
basil <basiljeannot@icloud.com>
2023-12-18 10:37:42 +0100

    norm valve_guy

--- srcs/load_screen/valve_guy.c
+++ srcs/load_screen/valve_guy.c
@@ -12,26 +12,19 @@
 
 #include "cub3D.h"
 
-static bool	is_pixel_valid(t_cub *cub, int x, int y, int color)
+static bool	is_pixel_valid(t_cub *cub, int x, int y)
 {
-	return (color >= 0 \
-		&& x < cub->win_size[WIDTH] \
+	return (x < cub->win_size[WIDTH] \
 		&& y < cub->win_size[HEIGHT]);
 }
 
-void	display_valve_guy(t_cub *cub)
+static void	upscale_to_screen_size(t_cub *cub, t_image valve_guy)
 {
-	t_image	valve_guy;
-	int		x;
-	int		y;
-	int		color;
-	int		texture_y;
-	int		texture_x;
+	int	texture_y;
+	int	texture_x;
+	int	y;
+	int	x;
 
-	valve_guy.img = mlx_xpm_file_to_image(cub->mlx, VALVE_GUY, &valve_guy.width, &valve_guy.height);
-	// if (!valve_guy.img)
-	// 	close_window(cub);
-	valve_guy.addr = mlx_get_data_addr(valve_guy.img, &valve_guy.bits_per_pixel, &valve_guy.line_length, &valve_guy.endian);
 	y = 0;
 	while (y < cub->win_size[HEIGHT])
 	{
@@ -40,15 +33,28 @@ void	display_valve_guy(t_cub *cub)
 		while (x < cub->win_size[WIDTH])
 		{
 			texture_x = x * valve_guy.width / cub->win_size[WIDTH];
-			color = *((int *)(valve_guy.addr + (texture_y * \
-				valve_guy.line_length + texture_x * \
-				(valve_guy.bits_per_pixel / 8))));
-			if (is_pixel_valid(cub, x, y, color))
-				cub_pixel_put(&cub->img, x, y, color);
+			if (is_pixel_valid(cub, x, y))
+				cub_pixel_put(&cub->img, x, y, \
+					*((int *)(valve_guy.addr + (texture_y * \
+						valve_guy.line_length + texture_x * \
+						(valve_guy.bits_per_pixel / 8)))));
 			x++;
 		}
 		y++;
 	}
+}
+
+void	display_valve_guy(t_cub *cub)
+{
+	t_image	valve_guy;
+
+	valve_guy.img = mlx_xpm_file_to_image(cub->mlx, VALVE_GUY, \
+		&valve_guy.width, &valve_guy.height);
+	if (!valve_guy.img)
+		close_window(cub);
+	valve_guy.addr = mlx_get_data_addr(valve_guy.img, \
+		&valve_guy.bits_per_pixel, &valve_guy.line_length, &valve_guy.endian);
+	upscale_to_screen_size(cub, valve_guy);
 	mlx_put_image_to_window(cub->mlx, cub->win, cub->img.img, 0, 0);
 	mlx_destroy_image(cub->mlx, valve_guy.img);
 	mlx_loop_hook(cub->mlx, init_game, cub);
